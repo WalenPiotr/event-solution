fd2c6dc046341fe99735c52f34a3632339514dee
Piotr Walen <walen.piotr@gmail.com>
2019-07-11 13:02:44 +0200

    Add client Put service into separate dir

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
+import ProTip from './ProTip';
+
+export default function App() {
+  return (
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Create React App v4-beta example with TypeScript
+        </Typography>
+        <ProTip />
+        <MadeWithLove />
+      </Box>
+    </Container>
+  );
+}
