1c39d3ab1bdf8c6d0945fde3c486475df97bbcfc
Piotr Walen <walen.piotr@gmail.com>
2019-07-18 18:27:56 +0200

    Change production config

--- client/src/clientConfig.ts
+++ client/src/clientConfig.ts
@@ -2,7 +2,7 @@ import { DefaultApi } from "./generated";
 
 const apiURL =
   process.env.NODE_ENV === "production"
-    ? "http://46.101.246.218:4000/api"
+    ? "http://46.101.246.218:4000"
     : "http://localhost:4000";
 
 export const createApiClient = () => new DefaultApi({ basePath: apiURL });
